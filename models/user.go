4697e98a00924f264bace95c5bd4433507580141
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2025-07-17 10:40:15 +0700

    feat: integrate controller with response

--- models/user.go
+++ models/user.go
@@ -1,6 +1,5 @@
 package models
 
 type User struct {
-	UserID string `gorm:"primaryKey;type:varchar(255)" json:"userId"`
-	Wallet string `gorm:"unique;type:varchar(50)" json:"wallet"`
+	WalletAddress string `gorm:"primaryKey;type:varchar(255)" json:"walletAddress"`
 }
